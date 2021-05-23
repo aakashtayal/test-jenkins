b9d5b9d8047fdda387e053da05edf84c8d108126
aakashtayal <aakashtayal11@gmail.com>
2021-05-23 19:30:29 +0000

    aa

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World HAHA!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World HAHAH!\"");
+            fail("\"message\" is not \"Hello World!\"");
         }
     }
 
